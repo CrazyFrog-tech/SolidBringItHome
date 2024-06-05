14250e18e40b6535e229f94ec3b90995deff429d
Martijn Prins <martijn.prins@rigd-loxia.nl>
2024-06-05 07:13:55 +0200

    reorder main class

--- src/main/java/org/solid/Main.java
+++ src/main/java/org/solid/Main.java
@@ -3,8 +3,9 @@ package org.solid;
 public class Main {
     public static void main(String[] args) {
         IShippingCalculator shippingCalculator = new ExpressShippingCalculator(10, 100);
-        IShippingCalculator shippingCalculator2 = new StandardShippingCalculator(10, 100);
         System.out.println("Express shipping cost: " + shippingCalculator.calculateShippingCost());
+
+        IShippingCalculator shippingCalculator2 = new StandardShippingCalculator(10, 100);
         System.out.println("Standard shipping cost: " + shippingCalculator2.calculateShippingCost());
     }
 }
